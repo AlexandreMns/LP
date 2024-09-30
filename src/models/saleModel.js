870766a979a0371b6cff5270acf44e4195b82b00
Vinicius-Jardim <153203354+Vinicius-Jardim@users.noreply.github.com>
2024-10-01 00:27:19 +0100

    sale model
    criado apenas schema de venda

---
+++ src/models/saleModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const SaleSchema = new mongoose.Schema({
+  property: { type: mongoose.Schema.Types.ObjectId, ref: "Property", required: true },
+  client: { type: mongoose.Schema.Types.ObjectId, ref: "Client", required: true },
+  agent: { type: mongoose.Schema.Types.ObjectId, ref: "Agent", required: true },
+  saleDate: { type: Date, default: Date.now, required: true },
+  saleValue: { type: Number, required: true },
+});
+
+const Sale = mongoose.model("Sale", SaleSchema);
+
+export default Sale;
